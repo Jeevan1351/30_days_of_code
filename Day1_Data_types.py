08c42d7f6fc4a2caac165bd75ff88134e159afc2
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-06-03 16:00:27 +0530

    Updates

---
+++ Day1_Data_types.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+this_integer = int(input())
+this_double = float(input())
+this_string = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(f"{this_integer+i}\n{this_double+d}\n{s+this_string}")
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
